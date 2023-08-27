7a73a3745998ed746adabc033acb925b9946b486
H Lee <yzz0728@gmail.com>
2023-08-28 08:52:51 +0900

    Fix jump error

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class Dinosaur():
         if self.dino_run:
             self.run()
         if self.dino_jump:
-            self.jump
+            self.jump()
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -68,18 +68,18 @@ class Dinosaur():
         if userInput[pygame.K_UP] and not self.dino_jump:
             self.dino_duck = False
             self.dino_jump = True
-            self.dino_rum = False
+            self.dino_run = False
         elif userInput[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_jump = False
-            self.dino_rum = False
+            self.dino_run = False
         elif not (self.dino_jump or userInput[pygame.K_DOWN]):
             self.dino_duck = False
             self.dino_jump = False
-            self.dino_rum = True
+            self.dino_run = True
 
     def duck(self):
-        self.image = self.duck_img
+        self.image = self.duck_img[0]  # 하나의 이미지 선택
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS_DUCK
@@ -97,9 +97,11 @@ class Dinosaur():
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
-        if self.jump_vel < - self.JUMP_VEL:
-            self.dino_jump = False
-            self.jump_vel = self.JUMP_VEL
+            if self.dino_rect.y >= self.Y_POS:  # 점프가 다 끝나면
+                self.dino_rect.y = self.Y_POS  # 다시 바닥으로 위치를 맞춤
+                self.jump_vel = self.JUMP_VEL
+                self.dino_jump = False
+
     
     def draw(self, SCREEN):
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
